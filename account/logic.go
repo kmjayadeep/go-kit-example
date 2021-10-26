3a003eb5672106261f01595a504da7458105adf2
Jayadeep KM <kmjayadeep@gmail.com>
2021-10-26 13:16:58 +0530

    add service logic
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- account/logic.go
+++ account/logic.go
@@ -1,29 +1,58 @@
 package account
 
 import (
-  "context"
+	"context"
 
-  "github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/log/level"
+	uuid "github.com/gofrs/uuid"
 )
 
 type service struct {
-  repository Repository
-  logger log.Logger
+	repository Repository
+	logger     log.Logger
 }
 
 func NewService(repo Repository, logger log.Logger) Service {
-  return &service{
-    repository: repo,
-    logger: logger,
-  }
+	return &service{
+		repository: repo,
+		logger:     logger,
+	}
 }
 
 func (s service) CreateUser(ctx context.Context, email, password string) (string, error) {
-  // TODO complete logic
-  return "", nil
+	logger := log.With(s.logger, "method", "CreateUser")
+
+	uuid, _ := uuid.NewV4()
+	id := uuid.String()
+
+	user := User{
+		Id:       id,
+		Email:    email,
+		Password: password,
+	}
+
+	if err := s.repository.CreateUser(ctx, user); err != nil {
+		level.Error(logger).Log("err", err)
+		return "", err
+	}
+
+	logger.Log("Create User", user.ID)
+
+	return user.ID, nil
 }
 
 func (s service) GetUser(ctx context.Context, id string) (string, error) {
-  // TODO complete logic
-  return "", nil
+	logger := log.With(s.logger, "method", "GetUser")
+
+	email, err := s.repository.GetUser(ctx, id)
+
+	if err != nil {
+		level.Error(logger).Log("err", err)
+		return "", err
+	}
+
+	logger.Log("Get user", id, "email", email)
+
+	return email, nil
 }
